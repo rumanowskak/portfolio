955da0f5676ec48007c71738506dc1b0e5f511bd
Jasiek <jasiekk9@gmail.com>
2022-04-18 20:47:24 +0200

    Recommit some directories

---
+++ 2048Clone/Lab2HW/resource.h
@@ -0,0 +1,38 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by Lab2HW.rc
+//
+#define IDC_MYICON                      2
+#define IDD_LAB2HW_DIALOG               102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_LAB2HW                      107
+#define IDI_SMALL                       108
+#define IDC_LAB2HW                      109
+#define IDR_MAINFRAME                   128
+#define IDI_ICON1                       130
+#define ID_TARGET_16                    32771
+#define ID_TARGET_64                    32772
+#define ID_TARGET_2048                  32773
+#define IDM_GAME                        32774
+#define IDM_NGAME                       32775
+#define IDM_8                           32776
+#define IDM_16                          32777
+#define IDM_64                          32778
+#define IDM_2048                        32779
+#define ID_ACCELERATOR32781             32781
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        131
+#define _APS_NEXT_COMMAND_VALUE         32783
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
